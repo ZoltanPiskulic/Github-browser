05bf74113a5617a3e5e51d7edc13fc6970519c0c
ZoltanPiskulic <thorzoltan+1@gmail.com>
2023-10-01 20:54:57 +0200

    Modified GitHub service and GitRepoDetails model

--- Github browser/Model/GitHubService.swift
+++ Github browser/Model/GitHubService.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import UIKit
 
 protocol ApiService {
 	func searchRepos(withQuery query: String, forPageNumber pageNumber: Int) async throws -> GitRepoSearchResultModel
-	func getRepoDetails(ofOwner ownerName: String, ofRepo repoName: String) async throws -> GitRepoDetails
+	func getRepoDetails(ofRepo repoFullName: String) async throws -> GitRepoDetails
+	func fetchProfilePicture(fromUrl url: String) async throws -> UIImage?
 }
 
 class GitHubService: ApiService {
@@ -22,7 +24,7 @@ class GitHubService: ApiService {
 	}
 
 	func searchRepos(withQuery query: String, forPageNumber pageNumber: Int) async throws -> GitRepoSearchResultModel {
-		guard let percentEncodedQuery = query.addingPercentEncoding(withAllowedCharacters: .whitespaces),
+		guard let percentEncodedQuery = query.addingPercentEncoding(withAllowedCharacters: .letters),
 				let url = URL(string: baseURL + Endpoints.repos.rawValue + "?q=\(percentEncodedQuery)&page=\(pageNumber)" ) else {
 			throw ServiceErrors.other
 		}
@@ -39,11 +41,16 @@ class GitHubService: ApiService {
 				throw ServiceErrors.other
 			}
 		}
-		return try JSONDecoder().decode(GitRepoSearchResultModel.self, from: data)
+		do {
+			return try JSONDecoder().decode(GitRepoSearchResultModel.self, from: data)
+		} catch {
+			throw ServiceErrors.other
+		}
+
 	}
 
-	func getRepoDetails(ofOwner ownerName: String, ofRepo repoName: String) async throws -> GitRepoDetails {
-		guard let url = URL(string: baseURL + Endpoints.repos.rawValue + "/\(ownerName)/\(repoName)" ) else {
+	func getRepoDetails(ofRepo repoFullName: String) async throws -> GitRepoDetails {
+		guard let url = URL(string: baseURL + Endpoints.repoDetails.rawValue + "/\(repoFullName)") else {
 			throw ServiceErrors.other
 		}
 
@@ -59,6 +66,29 @@ class GitHubService: ApiService {
 				throw ServiceErrors.other
 			}
 		}
-		return try JSONDecoder().decode(GitRepoDetails.self, from: data)
+
+		do {
+			return try JSONDecoder().decode(GitRepoDetails.self, from: data)
+		} catch {
+			throw ServiceErrors.other
+		}
+	}
+
+	func fetchProfilePicture(fromUrl url: String) async throws -> UIImage? {
+		guard let url = URL(string: url ) else {
+			throw ServiceErrors.other
+		}
+		let (data, response) = try await URLSession.shared.data(from: url)
+		guard let httpResponse = response as? HTTPURLResponse else {
+			throw ServiceErrors.other
+		}
+		guard httpResponse.statusCode == 200 else {
+			if let knownError = ServiceErrors(rawValue: httpResponse.statusCode) {
+				throw knownError
+			} else {
+				throw ServiceErrors.other
+			}
+		}
+		return UIImage(data: data)
 	}
 }
